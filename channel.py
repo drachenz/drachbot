c360ac71df39883c2163a978b38ad2cfbcccd65c
James Crawford <jamescrawford@gmail.com>
2017-12-19 17:07:07 -0500

    still working out how i want to handle channels

--- channel.py
+++ channel.py
@@ -1,13 +1,27 @@
+import bot
+
 # Channel class
 
 class Channel:
+    channels = []
+    num_channels = 0
 
-    def __init__(self, channel):
-        pass
+    def __init__(self, bot, channel):
+        self.channel = channel
+        self.bot = bot
+        self.on = False
+        self.error = "joining"
+        self.Join(self.channel)
 
     def __del__(self):
         pass
 
-    def SendMessage(self, text):
-        pass
+    def Send(self, text):
+        self.bot.SendPrivmsg(self.channel, text)
 
+    def Join(self, channel):
+        self.channels.append(channel)
+        num_channels += 1
+
+    def UpdateStatus(self, status, error):
+        pass
